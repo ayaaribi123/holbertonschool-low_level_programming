8a5eff4bcc852c979f71ddd095b85b75cffa1ea0
ayaaribi123 <ayaaribi11@gmail.com>
2022-06-08 13:11:28 -0700

    print alphat

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+char al;
+for (al = 'a'; al <= 'z'; al++)
+{
+if (al != 'e' &&  al != 'q')
+{
+putchar(al);
+}
+}
+putchar('\n');
+return (0);
+}
