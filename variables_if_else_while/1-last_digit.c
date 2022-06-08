67ab838ce02a71e982d00d4a4a54ebc51aff6be2
ayaaribi123 <ayaaribi11@gmail.com>
2022-06-08 02:01:21 -0700

    last digital

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -13,17 +13,17 @@ int num;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 num = n % 10;
-if (num > n)
+if (num > 5)
 {
-printf("the last digital of %d is %i  and  is greater than 5\n", n, num);
+printf("Last digit of %d is %i and is greater than 5\n", n, num);
 }
 else if (num == 0)
 {
-printf("the last digital of %d is %i  and  is 0\n", n, num);
+printf("Last digit of %d is %i and is 0\n", n, num);
 }
 else if (num < 6 && num != 0)
 {
-printf("the last digital of %d is %i and  is less than 6 and not 0\n", n, num);
+printf("Last digit of %d is %i and is less than 6 and not 0\n", n, num);
 }
 return (0);
 }
