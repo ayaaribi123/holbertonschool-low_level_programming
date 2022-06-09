1faa2b3a498484610d589c846173522ec9a61fd6
ayaaribi123 <ayaaribi11@gmail.com>
2022-06-09 16:21:09 +0200

    islower

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,13 +1,15 @@
 #include "main.h"
 /**
- *int _islower(int c) - check the code
+ * islower - check the code
  *
+ * @c: variable
  * Return: Always 0.
  */
 int _islower(int c)
 {
 if (c >= 97 && c <= 122)
-return (1);  
+return(1);  
 else
 return (0);
+
 }	
