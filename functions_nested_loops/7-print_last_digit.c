1e8100d00490059893c917baea799f80664df3c0
ayaaribi123 <ayaaribi11@gmail.com>
2022-06-09 23:21:13 +0200

    prints last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_last_digit - check the code
+*@a: variable
+*Return: Always 0.
+*/
+int print_last_digit(int a);
+{
+{
+if (n % 10;)
+return (n);
+else
+return (0);
+}
+}
