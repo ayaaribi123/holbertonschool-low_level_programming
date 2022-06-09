dc2614839fc2c80492e1344d7524b1142d94099c
ayaaribi123 <ayaaribi11@gmail.com>
2022-06-09 12:11:45 +0200

    alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
