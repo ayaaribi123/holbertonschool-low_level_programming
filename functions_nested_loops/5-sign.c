7e7d4635320dacafbe025ff50f4e2a1f640ad9a8
ayaaribi123 <ayaaribi11@gmail.com>
2022-06-09 22:20:20 +0200

    sings

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,11 +1,25 @@
 #include "main.h"
 /**
- * _sign - check the code
+ * print_sign - check the code
+ * 
  * @n: variable
  * Return: Always 0.
  */
 int print_sign(int n)
 {
-
-    
+    if (n > 0)
+{
+        return (1);
+        _putchar('+');
+}
+       else if (n == 0)
+{
+            return (0);
+         _putchar('+');
+}
+        else
+{ 
+             return (-1);
+            _putchar ('-');
+}
 }
