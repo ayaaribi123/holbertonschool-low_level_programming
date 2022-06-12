420691b1db885e106c15b05c912fcb7587b56800
ayaaribi123 <ayaaribi11@gmail.com>
2022-06-12 17:11:33 +0200

    mOST NUMBER

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*print_most_numbers - check the code
+*
+*Return: Always 0.
+*/
+void print_most_numbers(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+{
+if (n != 2 && n !=4)
+{
+putchar (n + '0');
+}
+}
+putchar ('\n');
+}
