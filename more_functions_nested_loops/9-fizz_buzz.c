f95a44b7e56e17455e2b0e29da94575f179b6800
ayaaribi123 <ayaaribi11@gmail.com>
2022-06-12 18:57:48 +0200

    fizz buzz*

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,9 +12,9 @@ for (a = 1; a<= 100; a++)
 {
 if ((a % 3 == 0 ) && (a % 5 == 0))
 printf("FizzBuzz");
-else (a % 3 == 0)
+else ((a % 3 == 0))
 printf("Fizz");
-else (a % 5 == 0)
+else ((a % 5 == 0))
 printf("buzz");
 else 
 printf ("%d" , a);
