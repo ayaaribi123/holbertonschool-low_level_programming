d6ca078f5c0acf6543e107978a449184e8cdb6d2
ayaaribi123 <ayaaribi11@gmail.com>
2022-06-12 17:30:30 +0200

    number

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*more_numbers - check the code
+*
+*Return: Always 0.
+*/
+void more_numbers(void)
+{
+int a;
+int b;
+for (a = 0; a < 10 ; i++)
+{
+for (b = 0 ; b < 15 ; b++)
+}
+putchar (a + '0');
+putchar (\n);
+}
