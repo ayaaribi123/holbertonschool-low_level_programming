c137339d2ab17ae1192a64e05742d10d6b4fe42c
ayaaribi123 <ayaaribi11@gmail.com>
2022-06-12 18:51:19 +0200

    fizz buzz*

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,24 +12,25 @@ for (a = 1; a<= 100; a++)
 {
 if ((a % 3 == 0 ) && (a % 5 == 0))
 {
-printf("FizzBuzz")
+printf("FizzBuzz");
 }
 else (a % 3 == 0)
 {
-printf("Fizz")
+printf("Fizz");
 }
 else (a % 5 == 0)
 {
-printf("buzz")
+printf("buzz");
 }
 else 
 {
-printf ("%d" , a)
+printf ("%d" , a);
 }
 else 
 {
-printf(" ")
+printf(" ");
 }
-printf ("\n")
+printf ("\n");
+return (0)
 }
 }
