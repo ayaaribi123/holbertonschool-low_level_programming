8c71132c9c3342b285545ff7b7d0a279a9ee2a8e
ayaaribi123 <ayaaribi11@gmail.com>
2022-06-14 10:01:00 +0200

    reset to 89

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*reset_to_98 - check the code
+*
+*@n: pointer
+*Return: Always 0.
+*/
+void reset_to_98(int *n)
+{
+int n;
+int *p; 
+n = 98;
+p = &n;
+}
