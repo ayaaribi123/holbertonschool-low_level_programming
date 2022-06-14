029cc39accca5115f3bcdaefca3ecafa59b93b38
ayaaribi123 <ayaaribi11@gmail.com>
2022-06-14 11:38:56 +0200

    strlen

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+*_strlen - check the code
+*
+*@s: pointer
+*Return: Always 0.
+*/
+int _strlen(char *s)
+{
+int c= 0
+while(*s!=’\0’)
+{
+c++;
+*s++;
+}
+Return (c);
+}
