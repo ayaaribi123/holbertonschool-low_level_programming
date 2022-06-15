49f4e16dc8d239e43653f5df8ece42f08fc37bb3
ayaaribi123 <ayaaribi11@gmail.com>
2022-06-15 09:14:52 +0200

    print rev

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -9,7 +9,12 @@
 void print_rev(char *s)
 {
 int i = 0;
-for (i = *s ; i >= 0 ; i--)
+_strlen(s);
+int length;
+length = _strlen(s);
+for (i = length ; i >= 0 ; i--)
+{
 putchar (*s);
-putchar('\n');
+}
+putchar (\n);
 }
