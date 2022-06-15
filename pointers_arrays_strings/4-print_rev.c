49561566d4e5dd84dd567890467eb4af6e12a85f
ayaaribi123 <ayaaribi11@gmail.com>
2022-06-15 08:51:26 +0200

    print rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*print_rev - check the code
+*
+*@s: pointer
+*Return: Always 0.
+*/
+void print_rev(char *s)
+{
+int i = 0 ;
+for(i=len;i>=0;i--);
+putchar (*s);
+putchar('\n');
+}
