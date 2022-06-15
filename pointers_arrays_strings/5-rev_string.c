f68cdf9926191027108140c628e2835b8f569455
ayaaribi123 <ayaaribi11@gmail.com>
2022-06-15 15:41:06 +0200

    fix

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,31 +1,31 @@
 #include "main.h"
 #include <string.h>
 /**
-*rev_string - check the code
-*
-*@s: pointer
-*Return: Always 0.
-*/
+ *rev_string - check the code
+ *
+ *@s: pointer
+ *Return: Always 0.
+ */
 void rev_string(char *s)
 {
-    int l, i;
-    char *begin_ptr, *end_ptr, temp;
- 
-    l = strlen(s);
- 
-    begin_ptr = s;
-    end_ptr = s;
- 
-    for (i = 0; i < l - 1; i++)
+	int l, i;
+	char *begin_ptr, *end_ptr, temp;
+
+	l = strlen(s);
+
+	begin_ptr = s;
+	end_ptr = s;
+
+	for (i = 0; i < l - 1; i++)
 		end_ptr++;
-	
-	for (i = 0; i < l / 2; i++) 
+
+	for (i = 0; i < l / 2; i++)
 	{
 		temp = *end_ptr;
-        *end_ptr = *begin_ptr;
-        *begin_ptr = temp;
+		*end_ptr = *begin_ptr;
+		*begin_ptr = temp;
 
-        begin_ptr++;
-        end_ptr--;
-    }
+		begin_ptr++;
+		end_ptr--;
+	}
 }
