963372d317a41d4f3cd17df97d2a7c2d156e5740
ayaaribi123 <ayaaribi11@gmail.com>
2022-06-15 09:20:42 +0200

    print rev

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 /**
 *print_rev - check the code
 *
@@ -11,7 +12,7 @@ void print_rev(char *s)
 int i = 0;
 int len;
 
-len = _strlen(s);
+len = strlen(s);
 for (i = len ; i >= 0 ; i--)
 {
 putchar (*s);
