6f3da3001a5191fd46ece5e36ccf47f044c02271
ayaaribi123 <ayaaribi11@gmail.com>
2022-06-16 21:36:26 +0200

    strcmp

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+*_strncpy - check the code
+*
+*@s1: pointer
+*@s2: pointer
+*Return: Always 0.
+*/
+int _strcmp(char *s1, char *s2)
+{
+	if (strlen(s1) == strlen(s2))
+		return (0);
+	else if (strlen(s1)<strlen(s2))
+		return (13);
+	else
+		return (-13);
+}
