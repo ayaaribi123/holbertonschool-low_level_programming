4564a39e74073ef2a4dd0cab76aee8ef24e7a889
ayaaribi123 <ayaaribi11@gmail.com>
2022-06-16 21:25:30 +0200

    strncpy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+*_strncpy - check the code
+*
+*@src: pointer
+*@dest: pointer
+*Return: Always 0.
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+char *start = dest;
+
+    while(*src != '\0')
+    {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+
+    *dest = '\0'; 
+    return start;
+}
