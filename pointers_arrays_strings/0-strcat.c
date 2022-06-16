3a49611f9194b88ff13c7e9534d27dc4faa65744
ayaaribi123 <ayaaribi11@gmail.com>
2022-06-16 20:31:34 +0200

    strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+*puts2 - check the code
+*
+*@src: pointer
+*@dest: pointer
+*Return: Always 0.
+*/
+char *_strcat(char *dest, char *src)
+{
+strcat(dest,src);
+	printf("\n%s\n",dest,src)
+
+    return 0;
+ }
