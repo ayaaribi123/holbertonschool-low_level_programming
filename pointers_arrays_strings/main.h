3bacb6d79eae34823d4effb50f9cd3f549e39d6a
ayaaribi123 <ayaaribi11@gmail.com>
2022-06-16 21:27:39 +0200

    main

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -8,3 +8,4 @@ void puts2(char *str);
 void print_array(int *a, int n);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
