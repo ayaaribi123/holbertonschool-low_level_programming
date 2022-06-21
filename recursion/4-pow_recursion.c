2812f5f08f4e6b003e904daafdc211998bd1b5c8
ayaaribi123 <ayaaribi11@gmail.com>
2022-06-21 16:18:51 +0200

    hihi

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <math.h>
+/**
+*_pow_recursion - check the code
+*@x: pointer
+*@y: pointer
+*Return: Always 0.
+*/
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+	return (-1);
+}
+if (y == 0)
+{
+	return (1);
+}
+	return (_pow_recursion(x, y-1) *x);
+}
