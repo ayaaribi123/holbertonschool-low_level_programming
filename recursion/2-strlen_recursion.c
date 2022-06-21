8b195ce70daabac9abc623381a764ea8603dc369
ayaaribi123 <ayaaribi11@gmail.com>
2022-06-21 15:08:45 +0200

    length

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -9,12 +9,12 @@
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-	{
-		return 0;
-	}
+{
+	return (0);
+}
 	else
-{ 
+{
 		s++;
-	return (_strlen_recursion(s)+1);
+	return (_strlen_recursion(s) + 1);
 }
 }
