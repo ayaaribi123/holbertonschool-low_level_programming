a199c4e8be1e2cf5e7ef2e7f789401731761c1d0
ayaaribi123 <ayaaribi11@gmail.com>
2022-06-21 15:05:34 +0200

    length

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+*_strlen_recursion - check the code
+*@s: pointer
+*Return: Always 0.
+*/
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return 0;
+	}
+	else
+{ 
+		s++;
+	return (_strlen_recursion(s)+1);
+}
+}
