a1c79b4ae5159add0ab1a736b16c824955eca4e9
ayaaribi123 <ayaaribi11@gmail.com>
2022-06-21 12:12:19 +0200

    reverse

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*_print_rev_recursion - check the code
+*@s: pointer
+*Return: Always 0.
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return ;
+	}
+	else
+	{
+		s++;
+		_print_rev_recursion(s);
+		_putchar(*s--);
+	}
+
+}
