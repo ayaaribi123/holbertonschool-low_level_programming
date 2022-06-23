a1fbfbb993a326be2e94631f87d4d76d7ea01fb3
ayaaribi123 <ayaaribi11@gmail.com>
2022-06-23 15:16:51 +0200

    whatsmyname

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,11 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * main - prints the name of the file
+ * @argc: count of args
+ * @argv: array of args
+ * Return: 0
+ */
+
 int main(int argc, char **argv)
 {
 	if (argc != 0)
 	{
 		printf("%s\n", argv[0]);
 	}
-	return 0;
+	return (0);
 }
