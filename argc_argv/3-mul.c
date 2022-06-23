15cc203849db6ab4de1ee7c1de4ff3fbf318b1f1
ayaaribi123 <ayaaribi11@gmail.com>
2022-06-23 17:19:50 +0200

    multiplay

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,17 +12,17 @@ int main(int argc, char **argv)
 {
 	int a, b;
 
-	a = atoi (argv [1]);
-	b = atoi (argv [2]);
+	a = atoi(argv [1]);
+	b = atoi(argv [2]);
 
 	{
 
 		if (argc < 3)
 	{
-			printf ("error");
+			printf("error");
 			return (1);
 	}
-else 
+		else
 	{
 			printf("%d\n", a * b);
 
