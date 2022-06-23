f5eb9a0d62a389855d32c67901cf6d5c4a47e789
ayaaribi123 <ayaaribi11@gmail.com>
2022-06-23 16:22:44 +0200

    args

---
+++ argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file
+ * @argc: count of args
+ * @argv: array of args
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int i ;
+	for (i = 0 ; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
