f8501940c44f0a4b54f56d1e17d89f05795f567d
ayaaribi123 <ayaaribi11@gmail.com>
2022-06-24 16:35:08 +0200

    strdup

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include"main.h"
+
+/**
+ * _strdup - check the code
+ * @str: character
+ * Return: 0
+ */
+
+char *_strdup(char *str)
+{
+	char *p;
+	int i, len = 0 ;
+
+	for(i = 0 ;str[i]; i++)
+		len++;
+
+	p = malloc((len) * sizeof(char));
+	
+		if (p == NULL)
+			return (NULL);
+
+		for (i = 0 ; i < len ; i++)
+			p[i] = str[i];
+		
+		p[len] ='\0';
+	return (p);
+}
