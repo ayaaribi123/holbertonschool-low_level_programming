3f33909e444992ecf2f45fda4a845a841dc4d6e1
ayaaribi123 <ayaaribi11@gmail.com>
2022-06-24 08:43:43 +0200

    add

---
+++ argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file
+ * @argc: count of args
+ * @argv: array of args
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+int a, b;
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+
+	{
+
+		if (argc < 3)
+	{
+			printf("error");
+			return (1);
+	}
+		else
+	{
+			printf("%d\n", a + b);
+
+	}
+	return (0);
+	}
+	}
