73abcda59200e530d856c1f74eb225a6d5ef35de
ayaaribi123 <ayaaribi11@gmail.com>
2022-06-24 16:39:16 +0200

    strdup

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -10,20 +10,20 @@
 
 char *_strdup(char *str)
 {
-	char *p;
-	int i, len = 0 ;
+char *p;
+int i, len = 0 ;
 
-	for(i = 0 ;str[i]; i++)
+	for(i = 0 ; str[i] ; i++)
 		len++;
 
-	p = malloc((len) * sizeof(char));
+p = malloc((len) * sizeof(char));
 	
 		if (p == NULL)
 			return (NULL);
 
 		for (i = 0 ; i < len ; i++)
-			p[i] = str[i];
+		p[i] = str[i];
 		
-		p[len] ='\0';
+		p[len] = '\0';
 	return (p);
 }
