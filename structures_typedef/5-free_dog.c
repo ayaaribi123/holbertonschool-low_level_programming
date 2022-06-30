3133869c4ea8124ffb05364a685bc4fa548651c9
ayaaribi123 <ayaaribi11@gmail.com>
2022-06-30 20:17:50 +0200

    free

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,12 +1,16 @@
 #include "dog.h"
-#include <stdio.h>
 #include <stdlib.h>
 /**
  * free_dog - check the code
- *
  * @d: dog
  * Return: Always 0.
  */
 void free_dog(dog_t *d)
 {
+	if (d == NULL)
+{
+		return;
+
+		free (d);
+}
 }
