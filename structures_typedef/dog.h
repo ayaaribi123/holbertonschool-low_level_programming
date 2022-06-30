f481de976382cbd4d2826103ee1805f924975886
ayaaribi123 <ayaaribi11@gmail.com>
2022-06-30 11:28:36 +0200

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -15,4 +15,17 @@ char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+/**
+ *dog_t - struct
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: the owner
+ */
+typedef struct dog_t
+{
+char *name;
+float age;
+char *owner;
+} dog_t;
+
 #endif
