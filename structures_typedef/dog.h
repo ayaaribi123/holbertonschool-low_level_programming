e3948abefa31c17ead5c8b545fe3d806117e7cbf
ayaaribi123 <ayaaribi11@gmail.com>
2022-06-30 10:34:53 +0200

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+ * struct_dog - struct
+ * @name: name of dog
+ * @age: age of dog 
+ *@owner: the owner 
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
