ab45378299f53b1d05437d832e9cb07a74259f6b
ayaaribi123 <ayaaribi11@gmail.com>
2022-06-30 20:06:51 +0200

    malloc

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,47 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * new_dog - check the code
+ * 
+ * @d: dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: Always 0.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+	
+	d=malloc(sizeof(dog_t));
+	if ((name == NULL)|| (age < 0) || (owner == NULL))
+	{
+		return (NULL);
+	}
+		if (d == NULL)
+		{
+			return (NULL);
+		}
+		if (d->name == NULL)
+		{
+			return (NULL);
+		}
+		if (d->age < 0)
+		{
+			return (NULL);
+		}
+		if (d->owner == NULL)
+		{
+			return (NULL);
+		}
+		if (d != NULL)
+		{
+			d->name = name;
+			d->age = age;
+			d->owner = owner;
+		}
+		free(d);
+		free(d->name);
+		free(d->owner);
+		return (d);
+}
