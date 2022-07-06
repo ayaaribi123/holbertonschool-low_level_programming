b553db56caefdf8a009ced29c2e24dcf377f91fd
ayaaribi123 <ayaaribi11@gmail.com>
2022-07-06 20:02:50 +0200

    calc func

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -13,15 +13,15 @@ int (*get_op_func(char *s))(int, int)
 		{"/", op_div},
 		{"%", op_mod},
 		{NULL, NULL}
-    };
-    int i;
+};
+int i;
 
 	i = 0;
 	while (ops[i].op)
 	{
-		if (strcmp(ops[i].op, s) == 0)
-				return (ops[i].f);
-			i++;
+if (strcmp(ops[i].op, s) == 0)
+	return (ops[i].f);
+		i++;
 	}
 	return (NULL);
 }
