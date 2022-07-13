fe0c14fe1249eb27a0017eeccd05bfdcb745c332
ayaaribi123 <ayaaribi11@gmail.com>
2022-07-13 20:26:39 +0200

    print element of list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* print_list - check the code
+*@h: the variable
+* Return: Always 0.
+*/
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+		printf("[%d] %s\n", h->len, h->str);
+		else
+		printf ("[0] (nil)`\n");
+		i++;
+		h = h->next;
+	}
+	return i;
+}
