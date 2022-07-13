de37f486d09cde5c6a52d193dce2e22567568405
ayaaribi123 <ayaaribi11@gmail.com>
2022-07-13 20:39:42 +0200

    print element of list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -15,9 +15,10 @@ size_t print_list(const list_t *h)
 		if (h->str != NULL)
 		printf("[%d] %s\n", h->len, h->str);
 		else
-		printf ("[0] (nil)`\n");
+		printf("[0] (nil)`\n");
 		i++;
+
 		h = h->next;
 	}
-	return i;
+	return (i);
 }
