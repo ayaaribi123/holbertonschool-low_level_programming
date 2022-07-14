a69359dad0aa9a48d435cf8736413a7e9ec45b8d
ayaaribi123 <ayaaribi11@gmail.com>
2022-07-14 18:17:18 +0200

    the node

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -14,10 +14,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 for (s = 0; s < index; s++)
 {
 if (head == NULL)
-{
 return (NULL);
 head = head->next;
-}
 }
 return (head);
 }
