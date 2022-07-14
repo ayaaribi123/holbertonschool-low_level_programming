a855b85d9244060656c8e205e1c204fda103004a
ayaaribi123 <ayaaribi11@gmail.com>
2022-07-14 13:33:20 +0200

    print

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+*function - check the code
+*Return: Always 0.
+*/
+void function(void)
+{
+	printf("You're beat! and yet, you must allow\n");
+	printf("I bore my house upon my back!\n");
+}
