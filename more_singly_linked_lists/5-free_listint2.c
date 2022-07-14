4ebe37859c2540c909270052ec7ba75bfa2bf0a6
ayaaribi123 <ayaaribi11@gmail.com>
2022-07-14 17:39:06 +0200

    free

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -10,12 +10,12 @@ void free_listint2(listint_t **head)
 {
 		listint_t *f;
 
-		if (head == NULL)
-				return;
-		while(*head != NULL)
+if (head == NULL)
+return;
+while (*head != NULL)
 		{
 			f = (*head)->next;
-			free (*head);
+			free(*head);
 			*head = f;
 		}
 		head = NULL;
