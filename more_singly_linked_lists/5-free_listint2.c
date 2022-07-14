11f005d06a2a3b2dd8da78f639b0417a03dc4a00
ayaaribi123 <ayaaribi11@gmail.com>
2022-07-14 17:33:53 +0200

    free

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+/**
+*free_listint2 - check the code
+*@head: head
+*Return: Always 0.
+*/
+void free_listint2(listint_t **head)
+{
+		listint_t *f;
+
+		if (head == NULL)
+				return;
+		{
+			f = (*head)->next;
+			free (*head);
+			*head = f;
+		}
+		head = NULL;
+}
