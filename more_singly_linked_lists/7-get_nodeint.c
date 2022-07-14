f5ad406de81ccc27aed35ef9458d7aa64e52e97f
ayaaribi123 <ayaaribi11@gmail.com>
2022-07-14 18:10:28 +0200

    the node

---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+/**
+*get_nodeint_at_index - check the code
+*@head: head
+*@index: variable
+*Return: Always 0.
+*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int s;
+
+	if (head == NULL)
+{
+			return (NULL);
+			head = head->next;
+}
+	for (s = 0; s < index; s++)
+	{
+		return (head);
+	}
+return (head);
+}
