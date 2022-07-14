bbfe5d9e3b0e80567583ce470bb421ecbc124303
ayaaribi123 <ayaaribi11@gmail.com>
2022-07-14 13:40:34 +0200

    print

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 #include <stdio.h>
+void function(void) __attribute__ ((constructor));
 /**
 *function - check the code
 *Return: Always 0.
 */
 void function(void)
 {
-	printf("You're beat! and yet, you must allow\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
