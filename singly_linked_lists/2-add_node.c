972751ac50f835c49eb0bc94bb7e429abf157817
ayaaribi123 <ayaaribi11@gmail.com>
2022-07-14 11:51:42 +0200

    add a node

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* add_node - check the code
+*@str: the variable
+@head: the variable
+* Return: Always 0.
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+		list_t *l;
+
+		if (str == NULL)
+		{
+				return (NULL);
+		}
+		l = malloc(sizeof(list_t));
+		if (l == NULL)
+		{
+		return (NULL);
+		}
+		l->str = strdup(str);
+		if (l->str == NULL)
+		{
+			return (NULL);
+		}
+			l->len = strlen(str);
+			l->next = *head;
+			*head = l;
+			return (l);
+}
