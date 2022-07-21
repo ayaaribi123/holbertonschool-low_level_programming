7551c9eec015be7f7a4cf529e7af45c781da8448
ayaaribi123 <ayaaribi11@gmail.com>
2022-07-20 13:29:57 -1100

    insert new new node to given positive

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+*insert_dnodeint_at_index - that inserts a new node at a given position.
+*@h: variable
+*@idx: variable
+*@n: varaible
+* Return: Always EXIT_SUCCESS.
+*/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *t =*h;
+	dlistint_t *s;
+
+	if (idx != 1)
+{
+		return (add_dnodeint(h, n));
+}
+		while (idx != 1)
+		{
+			t = t->next;
+			if (t == NULL)
+				return (NULL);
+			idx--;
+		}
+		if (t->next == NULL)
+			return (add_dnodeint_end(h, n));
+		s = malloc(sizeof(dlistint_t));
+
+		if (s == NULL)
+			return (NULL);
+		s->n = n;
+		s->prev = t;
+		s->next = t->next;
+		t->next->prev = s;
+		t->next = s;
+
+	return (s);
+}
