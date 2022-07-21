396c64e3d8cf3c4ad71dbde46a1a59f84c69526b
ayaaribi123 <ayaaribi11@gmail.com>
2022-07-21 18:12:37 +0100

    append a text a the end of file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content != NULL)
-		en = strlen (text_content);
+		en = strlen(text_content);
 
 o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
