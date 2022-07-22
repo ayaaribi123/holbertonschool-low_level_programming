bbc74091a0083ae3a18bde311c1dacda2d6d3199
ayaaribi123 <ayaaribi11@gmail.com>
2022-07-22 21:39:40 +0100

    cp

---
+++ file_io/3-cp.c
@@ -0,0 +1,56 @@
+#include "main.h"
+/**
+*append_text_to_file - a function that appends text at the end of file.
+*@filename: variable
+*@text_content: variable
+*Return: Always 0.
+*/
+int main (int argc, char **argv)
+{
+	int f, t, r, w;
+	char *c;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	f = open(argv[1], O_RDONLY);
+	if (f == -1 )
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n");
+		exit(98);
+	}
+	c = argv[2];
+	r = read(f, c, 1024);
+	t = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (t == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n");
+		exit(99);
+	}
+
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n");
+		exit(98);
+	}
+
+	w = write(t, c, r);
+	if (t == -1 || w == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n");
+		exit(99);
+	}
+	if (t == -1)
+	dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE\n");
+	if (f == -1)
+	dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE\n");
+	else 
+{
+	close(f);
+	close(t);
+}
+	return (0);
+}
