f90d984250cdfdfa6d4ad00e336e59375df6dd59
ayaaribi123 <ayaaribi11@gmail.com>
2022-07-22 23:39:14 +0100

    cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -10,7 +10,6 @@ int main(int argc, char **argv)
 	int f, t, r, w;
 	char *c;
 
-	c = malloc(sizeof(char) * 1024);
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
@@ -25,30 +24,24 @@ int main(int argc, char **argv)
 	c = argv[2];
 	r = read(f, c, 1024);
 	t = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
-	if (c == NULL)
+	if (t == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	if (t == -1)
-{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-}
 	if (r == -1)
-{
+	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %sn", argv[1]);
 		exit(98);
-}
+	}
 	w = write(t, c, r);
 	if (f == -1 || w == -1)
-{
+	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
-}
-	if (f == -1 || t == -1)
+	}
+	if (f == -1|| t == -1)
 	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f);
-	exit (100);
+	exit(100);
 	return (0);
 }
