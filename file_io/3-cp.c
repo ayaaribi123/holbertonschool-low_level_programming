65ff96ef15406f28416df8fc54a17c3d91809eba
ayaaribi123 <ayaaribi11@gmail.com>
2022-07-22 21:46:01 +0100

    cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -25,23 +25,29 @@ int main(int argc, char **argv)
 	r = read(f, c, 1024);
 	t = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (t == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n");
 		exit(99);
+	}
 	if (r == -1)
-		
+	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n");
 		exit(98);
+	}
 	w = write(t, c, r);
 	if (t == -1 || w == -1)
-
+	{
 		dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n");
 		exit(99);
+	}
 	if (t == -1)
 	dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE\n");
 	if (f == -1)
 	dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE\n");
 	else 
-{ close(f);
+{
+	close(f);
 	close(t);
-} return (0);
+}
+	return (0);
 }
