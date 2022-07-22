444bf6608db1c475dda44c2247d1c5d9160241e2
ayaaribi123 <ayaaribi11@gmail.com>
2022-07-22 23:54:46 +0100

    puts

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+*puts2 - check the code
+*
+*@str: pointer
+*Return: Always 0.
+*/
+void puts2(char *str) 
+{
+	int i;
+	int l = strlen(str);
+	
+	for (i = 0; i < l; i++)
+	{
+		if (i % 2 == 0)
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
