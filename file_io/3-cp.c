106c6591ae3f57135c3af66412a835541b103cc9
ayaaribi123 <ayaaribi11@gmail.com>
2022-07-23 11:47:23 +0100

    cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -40,7 +40,7 @@ int main(int argc, char **argv)
 	t = open(argv[2], O_WRONLY | O_APPEND, 1024);
 
 	while (r > 0);
-	}
+	
 	if (f == -1 || t == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f);
