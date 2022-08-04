08e0472de8cbfc86b7c49b7c2b8166fc5cebc283
ayaaribi123 <ayaaribi11@gmail.com>
2022-08-04 15:41:18 +0100

    hours and minute

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* print_last_digit - check the code
-*@a: variable
+* jack_bauer - check the code
 *Return: Always 0.
 */
 void jack_bauer(void)
@@ -12,12 +11,12 @@ for (m = 0; m < 60; m++)
 {
 for (h = 0; h < 24; h++)
 {
-		putchar(h / 10 +'0');
-		putchar(h % 10 + '0');
-		putchar(':');
-		putchar(m / 10 +'0');
-		putchar(m % 10 + '0'); 
-		putchar('\n');
+putchar(h / 10 + '0');
+putchar(h % 10 + '0');
+putchar(':');
+putchar(m / 10 + '0');
+putchar(m % 10 + '0'); 
+putchar('\n');
 }
 }
 }
