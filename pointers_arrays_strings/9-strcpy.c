71a0973c73e6b3f75802c72ce427633397144aed
ayaaribi123 <ayaaribi11@gmail.com>
2022-08-04 18:00:09 +0100

    strcpy

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+*_strcpy - check the code
+*@dest: pointer
+*@src: variable
+*Return: Always 0.
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; i < src[i]; i++)
+	{
+		dest[i] = src[i];
+	}
+	return ("\n");
+	return (dest);
+}
