88871c31f775957b2a783efc64797d6686b460f6
ayaaribi123 <ayaaribi11@gmail.com>
2022-08-05 17:37:03 +0100

    string toupper

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+ *string_toupper - check the code
+ *@s: pointer
+ *Return: Always 0.
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while( s[i] != '\0' )
+	{
+	if( s[i] >= 'a' && s[i] <= 'z' )
+        {
+           s[i] = s[i] - 32;
+        }
+		i++;
+}
+return (s);
+}
