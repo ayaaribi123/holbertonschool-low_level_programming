6d00ca1477ce00f4f2f799edfc17c78d80042d16
ayaaribi123 <ayaaribi11@gmail.com>
2022-08-05 18:57:15 +0100

    cap string

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -9,26 +9,26 @@ char *cap_string(char *s)
 {
 	int i;
 
-	for(i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if(i == 0)
+		if (i == 0)
 		{
-			if((s[i] >= 'a' && s[i] <= 'z'))
-			s[i] = s[i]-32;
+			if ((s[i] >= 'a' && s[i] <= 'z'))
+			s[i] = s[i] - 32;
 			continue;
 	}
-	if(s[i] == ' ')
+			if (s[i] == ' ')
 		{
 			++i;
-			if(s[i] >= 'a' && s[i] <= 'z')
+			if (s[i] >= 'a' && s[i] <= 'z')
 			{
-				s[i] = s[i]-32;
+				s[i] = s[i] - 32;
 				continue;
 			}
 			}
 		else
 		{
-			if(s[i] >= 'A' && s[i] <= 'Z')
+			if (s[i] >= 'A' && s[i] <= 'Z')
 				s[i] = s[i] + 32;
 		}
 }
