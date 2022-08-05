d580fd76935c567de44e94aeecab9cb4156d780a
ayaaribi123 <ayaaribi11@gmail.com>
2022-08-05 15:35:14 +0100

    rev array

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 /**
-*_strncpy - check the code
+*reverse_array - check the code
 *
 *@a: pointer
 *@n: pointer
@@ -12,6 +12,7 @@ void reverse_array(int *a, int n)
 {
 	int c;
 	int i;
+
 	for (i = 0; i < n ; i++)
 	{
 		n--;
