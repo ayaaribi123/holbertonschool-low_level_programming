7fa2654a282ba6bb4bcb45f4993a0f56fe76c0d0
ayaaribi123 <ayaaribi11@gmail.com>
2022-08-05 19:10:54 +0100

    cap string

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,11 +11,9 @@ char *cap_string(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (i == 0)
 		{
-			if ((s[i] >= 97 && s[i] <= 122))
+			if (s[i] >= 97 && s[i] <= 122)
 			s[i] = s[i] - 32;
-			continue;
 	}
 			if (s[i] == ' ')
 		{
