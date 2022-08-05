239575f6912298c0eec86aa26656aa0829f67e00
ayaaribi123 <ayaaribi11@gmail.com>
2022-08-05 16:12:57 +0100

    rev array

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -12,18 +12,11 @@ void reverse_array(int *a, int n)
 {
 	int c;
 	int i;
-	int b;
 
 	for (i = 0; i > n ; i--)
 	{
 		n = a[i];
 		a[i] = c;
-		c = n;
-	}
-	for (b = 1; i < b; b++)
-	{
-		n = a[b];
-		a[b] = c;
 		c = n;
 	}
 }
