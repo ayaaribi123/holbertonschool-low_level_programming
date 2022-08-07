e800f46ec8e793a33a369430a5a002da1f3278d5
ayaaribi123 <ayaaribi11@gmail.com>
2022-08-07 21:56:34 +0100

    leet

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ *leet - check the code
+ *@s: pointer
+ *Return: Always 0.
+ */
+char *leet(char *s)
+{
+	int i;
+	int a[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int num[] = {'4', '3', '0', '7', '1'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == a[i])
+		s[i] = num [i];
+	}
+	return (s);
+}
