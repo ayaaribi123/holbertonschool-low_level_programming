e65a9bbf56572a320e5dc14255f4f43ee61ec3bd
ayaaribi123 <ayaaribi11@gmail.com>
2022-08-10 22:31:33 +0100

    diagsums

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+*print_diagsums - check the code
+*
+*@a: int
+*@size: int
+*Return: Always 0.
+*/
+void print_diagsums(int *a, int size)
+{
+	 int d1 = 0, d2 = 0;
+	 int i;
+
+	for (i = 0; i < size; i++)
+    {
+        d1 += a[i];
+		a += size;
+	}
+		a -= size;
+	for (i = 0; i < size; i++)
+	{	
+        d2 += a[i];
+		a -= size;
+	}
+	printf ("%d, %d\n", d1, d2);
+}
