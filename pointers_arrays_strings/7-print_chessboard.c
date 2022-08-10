ff6923d5e3c80f2f5c0324a318747d9f7dba874c
ayaaribi123 <ayaaribi11@gmail.com>
2022-08-10 21:15:29 +0100

    chessboard

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+*print_chessboard - check the code
+*
+*@s: char
+*Return: Always 0.
+*/
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int p;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (p = 0; p < 8; p++)
+		{
+			putchar (a[i][p]);
+		}
+		putchar('\n');
+	}
+}
