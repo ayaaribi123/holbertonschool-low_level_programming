927c040a9262c1421840abde456b40ee8723b184
ayaaribi123 <ayaaribi11@gmail.com>
2022-08-10 17:32:50 +0100

    strpbrk

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+*_strpbrk - check the code
+*
+*@s: char
+*@accept: char
+*Return: Always 0.
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; s++)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+	}
+	return('\0');
+}
