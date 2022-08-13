5d332da45903343955749e4c0824bab8fd77afc8
ayaaribi123 <ayaaribi11@gmail.com>
2022-08-13 11:35:46 +0100

    8-24hours

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -7,16 +7,16 @@
 void jack_bauer(void)
 {
 int h, m;
-for (m = 0; m < 60; m++)
+for (h = 0; h <= 24; h++)
 {
-for (h = 0; h < 24; h++)
+for (m = 0; m <= 60; m++)
 {
-putchar(h / 10 + '0');
-putchar(h % 10 + '0');
-putchar(':');
-putchar(m / 10 + '0');
-putchar(m % 10 + '0');
-putchar('\n');
+_putchar((h / 10) + '0');
+_putchar((h % 10) + '0');
+_putchar(':');
+_putchar((m / 10) + '0');
+_putchar((m % 10) + '0');
+_putchar('\n');
 }
 }
 }
